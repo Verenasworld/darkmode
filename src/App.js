92ee5ad99fdb873c0ad95e8abe5335a09cef98fd
Verenasworld <verena.felizidas@gmail.com>
2021-01-29 13:51:07 +0100

    no working

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 
 import { useState } from "react";
 import Splash from "./Components/SplashScreen";
-import {ThemeProvider} from "styled-components";
-
+import { ThemeProvider } from "styled-components";
 
 const LightTheme = {
   pageBackground: "white",
   titleColor: "#dc658b",
   tagLineColor: "black"
-}
+};
+
 const DarkTheme = {
   pageBackground: "#282c36",
-  titleColor: "Lightpink",
+  titleColor: "lightpink",
   tagLineColor: "lavender"
 }
+
 const themes = {
   light: LightTheme,
   dark: DarkTheme,
 }
 
+
 function App() {
-  const [theme, setTheme] = useState("Light")
+  const [theme, setTheme] = useState("light")
   return (
-   <ThemeProvider theme = {themes[theme]}>
-     <Splash theme = {theme} setTheme = {setTheme}/>
-   </ThemeProvider>
+      <ThemeProvider theme={themes[theme]}>
+        <Splash theme={theme} setTheme={setTheme} />
+      </ThemeProvider>
   );
 }
 
--- src/Components/SplashScreen.js
+++ src/Components/SplashScreen.js
@@ -1,30 +1,29 @@
 import styled from "styled-components";
-import {CgSun} from "react-icons/all";
-import {HiMoon} from "react-icons/all";
-
+import { CgSun } from "react-icons/cg";
+import { HiMoon } from "react-icons/hi";
 
 const Toggle = styled.button`
-    cursor : pointer;
-    height: 50px; 
-    width: 50px;
+    cursor: pointer;
+    height: 50px;
+    width: 50px;   
     border-radius: 50%;
+    border: none;
     background-color: ${props => props.theme.titleColor};
-    color: ${props=>props.theme.pageBackground};
-    &:focus{
-    outline; none; 
+    color: ${props => props.theme.pageBackground};
+    &:focus {
+        outline: none;
     }
-    transition: all .5s ease; 
+    transition: all .5s ease;
 `;
 
-
 const Page = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    width: 100%;
-    background-color: ${props => props.theme.pageBackground};
-    transition: all.5s ease;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100%;
+  background-color: ${props => props.theme.pageBackground};
+  transition: all .5s ease;
 `;
 
 const Container = styled.div`
@@ -35,14 +34,14 @@ const Container = styled.div`
 
 const Title = styled.h1`
     color: ${props => props.theme.titleColor};
-    transition: all .5s ease; 
+    transition: all .5s ease;
 `;
 
-const Tagline = styled.span`
-    color : ${props => props.theme.titleColor};
-    transition: all .5s ease; 
- `;
-
+const TagLine = styled.span`
+    color: ${props => props.theme.tagLineColor};
+    font-size: 18px;
+    transition: all .5s ease;
+`;
 
 function Splash(props) {
     function changeTheme() {
@@ -51,19 +50,21 @@ function Splash(props) {
         } else {
             props.setTheme("light");
         }
-    }
+    };
 
-    const icon = props.theme === "light" ? <HiMoon size={40}/> : <CgSun size={40}/>;
+    const icon = props.theme === "light" ? <HiMoon size={40} /> : <CgSun size={40} />;
+
     return (
         <Page>
             <Container>
-                <Toggle onClick={changeTheme}>{icon}
+                <Toggle onClick={changeTheme}>
+                    {icon}
                 </Toggle>
-                <Title>Coding With Chain</Title>
-                <Tagline>Level up your web development skills</Tagline>
+                <Title>Coding With Chaim</Title>
+                <TagLine>Level up your web development skills!</TagLine>
             </Container>
         </Page>
     );
-}
+};
 
 export default Splash;
